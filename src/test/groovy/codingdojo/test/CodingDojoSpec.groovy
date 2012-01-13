3fefc41e8008bbaa1c61b3bf62b872b55ee03380
Javier Ramirez Jr <xbenek@dev.java.net>
2012-01-13 15:48:01 -0600

    Prueba Spock para las dos katas del Coding Dojo.

--- src/test/groovy/codingdojo/test/CodingDojoSpec.groovy
+++ src/test/groovy/codingdojo/test/CodingDojoSpec.groovy
@@ -1,12 +1,35 @@
-package codingdojo.test
+package codingdojo.test;
 
+import spock.lang.Specification
+import codingdojo.romanos.Romanos
+import codingdojo.numerosprimos.NumerosPrimos
 /**
  * @author benek
  * Date: 13/01/12
- * Time: 15:05
+ * Time: 00:32
  * www.javamexico.org
  */
-import spock.lang.Specification;
-
 class CodingDojoSpec extends Specification{
+
+    def "Prueba de conversion de numero romano a arabigo"(){
+        given:
+            Romanos romanos = new Romanos();
+        when:
+            def conversion = romanos.romanoArabigo(romanoEntrada);
+        then:
+            conversion == resultadoArabigo;
+        where:
+            romanoEntrada << ["MMXX", "MCCXXXIV", "CMXCIX", "MMMDCCCXCIX", "XVIII", "MCMXCIX"];
+            resultadoArabigo << [2020, 1234, 999, 3899, 18, 1999];
+    }
+
+    def "Prueba de obtencion de un numero primo"(){
+        given:
+            NumerosPrimos numerosPrimos = new NumerosPrimos();
+        when:
+            def resultado = numerosPrimos.numeroPrimo(10001);
+        then:
+            resultado == 104743;
+    }
+
 }
